994e0d175e7a6f2578a954c28d2e973e461e83aa
ghazalasehar <ghazalasehar333@gmail.com>
2025-08-25 18:42:09 +0500

    new files

---
+++ laravel_eight/resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+   <x-slot name="title">Home</x-slot>
+
+    <x-slot name="main">
+        <div>
+        <h1>home page heading</h1>
+        <h3>Subheading for home page</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem tempore ipsum quibusdam eius possimus recusandae officiis harum suscipit nisi velit. Reiciendis maiores saepe placeat illo totam aliquid qui dolore. Non!</p>
+    </div>
+      </x-slot>
+   </x-layout>
---
+++ laravel_eight/resources/views/login.blade.php
@@ -0,0 +1,14 @@
+<x-layout>
+   <x-slot name="title">login page</x-slot>
+
+    <x-slot name="main">
+        <h1>User login</h1>
+        <input type="text" name="name" placeholder="enter name">
+        <br>
+        <br>
+         <input type="text" name="password" placeholder="enter password">
+        <br>
+        <br>
+        <button>Login</button>
+      </x-slot>
+   </x-layout>

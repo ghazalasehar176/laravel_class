adcfc653b0a2e238655065f7b6509364843c0d7a
ghazalasehar <ghazalasehar333@gmail.com>
2025-08-26 13:51:44 +0500

    new file

---
+++ laravel_eight/app/Http/Controllers/Stub2controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Stub2controller extends Controller
+{
+    function index(){
+        return "some text";
+    }
+}
---
+++ laravel_eight/app/Http/Controllers/Stubcontroller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Stubcontroller extends Controller
+{
+    function index(){
+        return "some text";
+    }
+}
